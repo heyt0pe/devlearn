c28767366700ae877f93cd3402fb4652d4d5f573
heyt0pe <akinthope@gmail.com>
2023-05-23 15:55:53 +0100

    initial

--- script.js
+++ script.js
@@ -1,98 +1,109 @@
-// let firstName = "Elvis";
-// const gender = "Female";
-// var nigerian = true;
+// // let firstName = "Elvis";
+// // const gender = "Female";
+// // var nigerian = true;
 
-// console.log(firstName, gender, nigerian);
+// // console.log(firstName, gender, nigerian);
 
-// let num = 10;
-// console.log(num)
-// num = num + 3;
-// console.log(num)
-// num += 2
-// console.log(num)
-// num *= 2
-// console.log(num)
-// num /= 5
-// console.log(num)
-// num -= 6
-// console.log(num)
-// num = 3
-// console.log(num)
-// console.log(num % 2)
-// num += 3
-// console.log(num)
-// console.log(num % 2)
-// // console.log('')
+// // let num = 10;
+// // console.log(num)
+// // num = num + 3;
+// // console.log(num)
+// // num += 2
+// // console.log(num)
+// // num *= 2
+// // console.log(num)
+// // num /= 5
+// // console.log(num)
+// // num -= 6
+// // console.log(num)
+// // num = 3
+// // console.log(num)
+// // console.log(num % 2)
+// // num += 3
+// // console.log(num)
+// // console.log(num % 2)
+// // // console.log('')
 
-// let str1 = "Hello"
+// // let str1 = "Hello"
 
-// let str3 = "Good";
-// let str4 = "Morning";
+// // let str3 = "Good";
+// // let str4 = "Morning";
 
-// let str5 = str1 + ', ' + str3 + ' ' + str4
-// // console.log(str5)
+// // let str5 = str1 + ', ' + str3 + ' ' + str4
+// // // console.log(str5)
 
 
 
-// let str6 = `str1, str3 str4`;
-// console.log(str6)
+// // let str6 = `str1, str3 str4`;
+// // console.log(str6)
 
-// let age = 14
+// // let age = 14
 
-// if (age > 17) {
-//     console.log('Grant drivers license');
+// // if (age > 17) {
+// //     console.log('Grant drivers license');
+// // } else {
+// //     // console.log("You're too young, come back in " + (18 - age) + " years time")
+// //     console.log(`You're too young, come back in ${18 - age} years time`)
+// // }
+
+
+// // let time = 10;
+
+// // if (time < 12) {
+// //     console.log("The time is currenot time. " + `Good morning. it is ${12 - time} hours till afternoon`);
+// // } else if (time < 18) {
+// //     console.log('Good afternoon, it is x till evening');
+// // } else {
+// //     console.log('Good evening.It is x hours till midnight');
+// // }
+
+
+// // `
+// // Percentage >= 90% : Grade A
+// // Percentage >= 80% : Grade B
+// // Percentage >= 70% : Grade C
+// // Percentage >= 60% : Grade D
+// // Percentage >= 40% : Grade E
+// // Percentage < 40% : Grade F
+// // `
+// // console.log(
+// //     "Your grade is C. You needed 17 more point(s) to get a B"
+// // );
+// // console.log(
+// //     "Your grade is A. You are 67 pointsa above the failing grade"
+// // );
+
+
+
+
+
+
+
+
+// let num = 621;
+
+
+// if (num % 2 == 0) {
+//     if (num < 100) {
+//         console.log(`${num} is a small even number`);
+//     } else {
+//         console.log(`${num} is a big even number`);
+//     }
 // } else {
-//     // console.log("You're too young, come back in " + (18 - age) + " years time")
-//     console.log(`You're too young, come back in ${18 - age} years time`)
+//     if (num < 100) {
+//         console.log(`${num} is a small odd number`);
+//     } else {
+//         console.log(`${num} is a big odd number`);
+//     }
 // }
 
-
-// let time = 10;
-
-// if (time < 12) {
-//     console.log("The time is currenot time. " + `Good morning. it is ${12 - time} hours till afternoon`);
-// } else if (time < 18) {
-//     console.log('Good afternoon, it is x till evening');
-// } else {
-//     console.log('Good evening.It is x hours till midnight');
-// }
-
-
-// `
-// Percentage >= 90% : Grade A
-// Percentage >= 80% : Grade B
-// Percentage >= 70% : Grade C
-// Percentage >= 60% : Grade D
-// Percentage >= 40% : Grade E
-// Percentage < 40% : Grade F
-// `
-// console.log(
-//     "Your grade is C. You needed 17 more point(s) to get a B"
-// );
-// console.log(
-//     "Your grade is A. You are 67 pointsa above the failing grade"
-// );
+let a = "Hello";
+let s = ' ';
+let b = "World";
+let t = 7
+console.log('Good morning, it is '+(12 -t) +' hours till afternoon')
+console.log('Good morning, it is ${12-t} hours till afternoon')
+console.log(`Good morning, it is ${12-t} hours till afternoon`)
 
 
 
-
-
-
-
-
-let num = 621;
-
-
-if (num % 2 == 0) {
-    if (num < 100) {
-        console.log(`${num} is a small even number`);
-    } else {
-        console.log(`${num} is a big even number`);
-    }
-} else {
-    if (num < 100) {
-        console.log(`${num} is a small odd number`);
-    } else {
-        console.log(`${num} is a big odd number`);
-    }
-}
