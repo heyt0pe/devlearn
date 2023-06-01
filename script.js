37619a727cb9ceef603b40811a49e135bff02a2c
heyt0pe <akinthope@gmail.com>
2023-06-01 17:46:57 +0100

    loops

--- script.js
+++ script.js
@@ -1,109 +1,336 @@
-// // let firstName = "Elvis";
-// // const gender = "Female";
-// // var nigerian = true;
+// // // let firstName = "Elvis";
+// // // const gender = "Female";
+// // // var nigerian = true;
 
-// // console.log(firstName, gender, nigerian);
+// // // console.log(firstName, gender, nigerian);
 
-// // let num = 10;
-// // console.log(num)
-// // num = num + 3;
-// // console.log(num)
-// // num += 2
-// // console.log(num)
-// // num *= 2
-// // console.log(num)
-// // num /= 5
-// // console.log(num)
-// // num -= 6
-// // console.log(num)
-// // num = 3
-// // console.log(num)
-// // console.log(num % 2)
-// // num += 3
-// // console.log(num)
-// // console.log(num % 2)
-// // // console.log('')
+// // // let num = 10;
+// // // console.log(num)
+// // // num = num + 3;
+// // // console.log(num)
+// // // num += 2
+// // // console.log(num)
+// // // num *= 2
+// // // console.log(num)
+// // // num /= 5
+// // // console.log(num)
+// // // num -= 6
+// // // console.log(num)
+// // // num = 3
+// // // console.log(num)
+// // // console.log(num % 2)
+// // // num += 3
+// // // console.log(num)
+// // // console.log(num % 2)
+// // // // console.log('')
 
-// // let str1 = "Hello"
+// // // let str1 = "Hello"
 
-// // let str3 = "Good";
-// // let str4 = "Morning";
+// // // let str3 = "Good";
+// // // let str4 = "Morning";
 
-// // let str5 = str1 + ', ' + str3 + ' ' + str4
-// // // console.log(str5)
+// // // let str5 = str1 + ', ' + str3 + ' ' + str4
+// // // // console.log(str5)
 
 
 
-// // let str6 = `str1, str3 str4`;
-// // console.log(str6)
+// // // let str6 = `str1, str3 str4`;
+// // // console.log(str6)
 
-// // let age = 14
+// // // let age = 14
 
-// // if (age > 17) {
-// //     console.log('Grant drivers license');
+// // // if (age > 17) {
+// // //     console.log('Grant drivers license');
+// // // } else {
+// // //     // console.log("You're too young, come back in " + (18 - age) + " years time")
+// // //     console.log(`You're too young, come back in ${18 - age} years time`)
+// // // }
+
+
+// // // let time = 10;
+
+// // // if (time < 12) {
+// // //     console.log("The time is currenot time. " + `Good morning. it is ${12 - time} hours till afternoon`);
+// // // } else if (time < 18) {
+// // //     console.log('Good afternoon, it is x till evening');
+// // // } else {
+// // //     console.log('Good evening.It is x hours till midnight');
+// // // }
+
+
+// // // `
+// // // Percentage >= 90% : Grade A
+// // // Percentage >= 80% : Grade B
+// // // Percentage >= 70% : Grade C
+// // // Percentage >= 60% : Grade D
+// // // Percentage >= 40% : Grade E
+// // // Percentage < 40% : Grade F
+// // // `
+// // // console.log(
+// // //     "Your grade is C. You needed 17 more point(s) to get a B"
+// // // );
+// // // console.log(
+// // //     "Your grade is A. You are 67 pointsa above the failing grade"
+// // // );
+
+
+
+
+
+
+
+
+// // let num = 621;
+
+
+// // if (num % 2 == 0) {
+// //     if (num < 100) {
+// //         console.log(`${num} is a small even number`);
+// //     } else {
+// //         console.log(`${num} is a big even number`);
+// //     }
 // // } else {
-// //     // console.log("You're too young, come back in " + (18 - age) + " years time")
-// //     console.log(`You're too young, come back in ${18 - age} years time`)
+// //     if (num < 100) {
+// //         console.log(`${num} is a small odd number`);
+// //     } else {
+// //         console.log(`${num} is a big odd number`);
+// //     }
 // // }
 
-
-// // let time = 10;
-
-// // if (time < 12) {
-// //     console.log("The time is currenot time. " + `Good morning. it is ${12 - time} hours till afternoon`);
-// // } else if (time < 18) {
-// //     console.log('Good afternoon, it is x till evening');
-// // } else {
-// //     console.log('Good evening.It is x hours till midnight');
-// // }
-
-
-// // `
-// // Percentage >= 90% : Grade A
-// // Percentage >= 80% : Grade B
-// // Percentage >= 70% : Grade C
-// // Percentage >= 60% : Grade D
-// // Percentage >= 40% : Grade E
-// // Percentage < 40% : Grade F
-// // `
-// // console.log(
-// //     "Your grade is C. You needed 17 more point(s) to get a B"
-// // );
-// // console.log(
-// //     "Your grade is A. You are 67 pointsa above the failing grade"
-// // );
+// let a = "Hello";
+// let s = ' ';
+// let b = "World";
+// let t = 7
+// console.log('Good morning, it is ' + (12 - t) + ' hours till afternoon')
+// console.log('Good morning, it is ${12-t} hours till afternoon')
+// console.log(`Good morning, it is ${12 - t} hours till afternoon`)
 
 
 
 
 
 
+// // age = 12
+// // gender = 'male'
+
+// // below 18
+// // male - fanta
+// // feale - coke
+
+// // above 18
+// // male - wine
+// // fenale - beer
 
 
-// let num = 621;
+// console.log('you are welcome, you can have a fanta')
+// console.log('you are welcome, you can have a coke')
 
 
-// if (num % 2 == 0) {
-//     if (num < 100) {
-//         console.log(`${num} is a small even number`);
-//     } else {
-//         console.log(`${num} is a big even number`);
-//     }
-// } else {
-//     if (num < 100) {
-//         console.log(`${num} is a small odd number`);
-//     } else {
-//         console.log(`${num} is a big odd number`);
-//     }
+
+
+
+// // Hello you are alloweed to take alcohol. Ple`se take a bottle of wine
+
+
+
+// // 
+// // isAlive = true/false
+// // dob = 1995
+// // 
+// // 
+// // if alive
+// // < 13 - you are alive. child
+// // 13 - 19 you are alive. teenager
+// // >19 - you are alive. adult
+
+// // < 13 - you are dead. child
+// // 13 - 19 you are dead. teenager
+// // >19 - you are dead. adult
+
+
+// // index 0 - name
+// // index 1 - age
+// // index 2 - gender
+// let person1 = [
+//     'Tope',
+//     12,
+//     'Male',
+//     ['Tope', 21],
+//     { firstName: 'Tope' },
+// ]
+// let person2 = ['Tobi', 18, 'Female']
+// let person3 = ['Elvis', 40, 'Male']
+
+// let person4 = {
+//     firstName: "Elvis",
+//     age: 40,
+//     gender: "Male",
+//     children: [
+//         {
+//             firstName: 'Tope',
+//             age: 10,
+//             schools: ['KJA', 'KLC'],
+//         },
+//         {
+//             firstName: 'Jimi',
+//             age: 3,
+//             schools: null,
+//         }
+//     ]
 // }
 
-let a = "Hello";
-let s = ' ';
-let b = "World";
-let t = 7
-console.log('Good morning, it is '+(12 -t) +' hours till afternoon')
-console.log('Good morning, it is ${12-t} hours till afternoon')
-console.log(`Good morning, it is ${12-t} hours till afternoon`)
+let arr = [
+    'first',
+    2,
+    [
+        false,
+        {
+            key: 'value',
+            data: [
+                67,
+                78,
+                {
+                    schools: ['test', 'lemon'],
+                },
+            ],
+        },
+    ]
+]
+
+// console.log(arr[2][1].data[2].schools[1])
+
+
+
+
+// let arr = [
+//     3, 
+//     '2', 
+//     { 
+//         "age": 14, 
+//         "car": "Toyota" 
+//     }, 
+//     [
+//         4, 
+//         7, 
+//         [
+//             "first", 
+//             "second", 
+//             { 
+//                 "position": "third", 
+//                 "class": "JSS1C", 
+//             },
+//         ],
+//     ],
+// ]
+
+
+// console.log(typeof(arr[2].age))
+
+
+
+
+// let people = ['Tope', 'Paul', "Tobi", "Rita", "Elvis"]
+
+// console.log(people[0])
+// console.log(people[1])
+// console.log(people[2])
+// console.log(people[3])
+// console.log(people[4])
+
+
+
+
+
+
+
+
+
+
+
+// let u = 1;
+
+
+// for(i = 0; i < 10; i+2){
+//     console.log(i)
+// }
+
+
+
+
+
+
+// console.log('Hello Tope')
+
+
+
+
+
+let index = 13
+
+
+
+
+
+
+
+
+
+
+// console.log(`arr[${index}]`)
+
+
+
+
+
+
+// console.log('arr[x]')
+
+
+
+
+
+
+
+
+// let grade = 79
+
+// console.log('arr[' + (80 - grade) + ']')
+
+
+let firstName = "John"
+let greeting = 'Hey'
+
+// console.log(greeting + ' ' + firstName)
+// console.log(`${greeting} ${firstName}`)
+
+index = 12
+
+
+
+
+
+
+
+
+let a = ['Tope', "Paul", "Jimi", "John", 'Mary']
+
+
+for (i = 0; i < 5; i++) {
+    if (i % 2 == 0) {
+        console.log(a[i])
+    }
+}
+
+console.log(' ')
+
+for (i = 0; i < 5; i=i+2) {
+    console.log(a[i])
+}
+
+
+
+
+
 
 
 
