1350b3de371fed199aa5be712ced2af41bd4fe7b
Temitope <akinthope@gmail.com>
2023-08-22 18:31:54 +0100

    js styling

--- script.js
+++ script.js
@@ -1,326 +1,326 @@
-// // // let firstName = "Elvis";
-// // // const gender = "Female";
-// // // var nigerian = true;
+// // // // let firstName = "Elvis";
+// // // // const gender = "Female";
+// // // // var nigerian = true;
 
-// // // console.log(firstName, gender, nigerian);
+// // // // console.log(firstName, gender, nigerian);
 
-// // // let num = 10;
-// // // console.log(num)
-// // // num = num + 3;
-// // // console.log(num)
-// // // num += 2
-// // // console.log(num)
-// // // num *= 2
-// // // console.log(num)
-// // // num /= 5
-// // // console.log(num)
-// // // num -= 6
-// // // console.log(num)
-// // // num = 3
-// // // console.log(num)
-// // // console.log(num % 2)
-// // // num += 3
-// // // console.log(num)
-// // // console.log(num % 2)
-// // // // console.log('')
+// // // // let num = 10;
+// // // // console.log(num)
+// // // // num = num + 3;
+// // // // console.log(num)
+// // // // num += 2
+// // // // console.log(num)
+// // // // num *= 2
+// // // // console.log(num)
+// // // // num /= 5
+// // // // console.log(num)
+// // // // num -= 6
+// // // // console.log(num)
+// // // // num = 3
+// // // // console.log(num)
+// // // // console.log(num % 2)
+// // // // num += 3
+// // // // console.log(num)
+// // // // console.log(num % 2)
+// // // // // console.log('')
 
-// // // let str1 = "Hello"
+// // // // let str1 = "Hello"
 
-// // // let str3 = "Good";
-// // // let str4 = "Morning";
+// // // // let str3 = "Good";
+// // // // let str4 = "Morning";
 
-// // // let str5 = str1 + ', ' + str3 + ' ' + str4
-// // // // console.log(str5)
+// // // // let str5 = str1 + ', ' + str3 + ' ' + str4
+// // // // // console.log(str5)
 
-// // // let str6 = `str1, str3 str4`;
-// // // console.log(str6)
+// // // // let str6 = `str1, str3 str4`;
+// // // // console.log(str6)
 
-// // // let age = 14
+// // // // let age = 14
 
-// // // if (age > 17) {
-// // //     console.log('Grant drivers license');
+// // // // if (age > 17) {
+// // // //     console.log('Grant drivers license');
+// // // // } else {
+// // // //     // console.log("You're too young, come back in " + (18 - age) + " years time")
+// // // //     console.log(`You're too young, come back in ${18 - age} years time`)
+// // // // }
+
+// // // // let time = 10;
+
+// // // // if (time < 12) {
+// // // //     console.log("The time is currenot time. " + `Good morning. it is ${12 - time} hours till afternoon`);
+// // // // } else if (time < 18) {
+// // // //     console.log('Good afternoon, it is x till evening');
+// // // // } else {
+// // // //     console.log('Good evening.It is x hours till midnight');
+// // // // }
+
+// // // // `
+// // // // Percentage >= 90% : Grade A
+// // // // Percentage >= 80% : Grade B
+// // // // Percentage >= 70% : Grade C
+// // // // Percentage >= 60% : Grade D
+// // // // Percentage >= 40% : Grade E
+// // // // Percentage < 40% : Grade F
+// // // // `
+// // // // console.log(
+// // // //     "Your grade is C. You needed 17 more point(s) to get a B"
+// // // // );
+// // // // console.log(
+// // // //     "Your grade is A. You are 67 pointsa above the failing grade"
+// // // // );
+
+// // // let num = 621;
+
+// // // if (num % 2 == 0) {
+// // //     if (num < 100) {
+// // //         console.log(`${num} is a small even number`);
+// // //     } else {
+// // //         console.log(`${num} is a big even number`);
+// // //     }
 // // // } else {
-// // //     // console.log("You're too young, come back in " + (18 - age) + " years time")
-// // //     console.log(`You're too young, come back in ${18 - age} years time`)
+// // //     if (num < 100) {
+// // //         console.log(`${num} is a small odd number`);
+// // //     } else {
+// // //         console.log(`${num} is a big odd number`);
+// // //     }
 // // // }
 
-// // // let time = 10;
+// // let a = "Hello";
+// // let s = ' ';
+// // let b = "World";
+// // let t = 7
+// // console.log('Good morning, it is ' + (12 - t) + ' hours till afternoon')
+// // console.log('Good morning, it is ${12-t} hours till afternoon')
+// // console.log(`Good morning, it is ${12 - t} hours till afternoon`)
 
-// // // if (time < 12) {
-// // //     console.log("The time is currenot time. " + `Good morning. it is ${12 - time} hours till afternoon`);
-// // // } else if (time < 18) {
-// // //     console.log('Good afternoon, it is x till evening');
-// // // } else {
-// // //     console.log('Good evening.It is x hours till midnight');
-// // // }
+// // // age = 12
+// // // gender = 'male'
 
-// // // `
-// // // Percentage >= 90% : Grade A
-// // // Percentage >= 80% : Grade B
-// // // Percentage >= 70% : Grade C
-// // // Percentage >= 60% : Grade D
-// // // Percentage >= 40% : Grade E
-// // // Percentage < 40% : Grade F
-// // // `
-// // // console.log(
-// // //     "Your grade is C. You needed 17 more point(s) to get a B"
-// // // );
-// // // console.log(
-// // //     "Your grade is A. You are 67 pointsa above the failing grade"
-// // // );
+// // // below 18
+// // // male - fanta
+// // // feale - coke
 
-// // let num = 621;
+// // // above 18
+// // // male - wine
+// // // fenale - beer
 
-// // if (num % 2 == 0) {
-// //     if (num < 100) {
-// //         console.log(`${num} is a small even number`);
-// //     } else {
-// //         console.log(`${num} is a big even number`);
+// // console.log('you are welcome, you can have a fanta')
+// // console.log('you are welcome, you can have a coke')
+
+// // // Hello you are alloweed to take alcohol. Ple`se take a bottle of wine
+
+// // //
+// // // isAlive = true/false
+// // // dob = 1995
+// // //
+// // //
+// // // if alive
+// // // < 13 - you are alive. child
+// // // 13 - 19 you are alive. teenager
+// // // >19 - you are alive. adult
+
+// // // < 13 - you are dead. child
+// // // 13 - 19 you are dead. teenager
+// // // >19 - you are dead. adult
+
+// // // index 0 - name
+// // // index 1 - age
+// // // index 2 - gender
+// // let person1 = [
+// //     'Tope',
+// //     12,
+// //     'Male',
+// //     ['Tope', 21],
+// //     { firstName: 'Tope' },
+// // ]
+// // let person2 = ['Tobi', 18, 'Female']
+// // let person3 = ['Elvis', 40, 'Male']
+
+// // let person4 = {
+// //     firstName: "Elvis",
+// //     age: 40,
+// //     gender: "Male",
+// //     children: [
+// //         {
+// //             firstName: 'Tope',
+// //             dob: 2000,
+// //             schools: ['KJA', 'KLC'],
+// //         },
+// //         {
+// //             firstName: 'Jimi',
+// //             age: 3,
+// //             schools: null,
+// //         }
+// //     ]
+// // }
+
+// // let arr = [
+// //     'first',
+// //     2,
+// //     [
+// //         false,
+// //         {
+// //             key: 'value',
+// //             data: [
+// //                 67,
+// //                 78,
+// //                 {
+// //                     schools: ['test', 'lemon'],
+// //                 },
+// //             ],
+// //         },
+// //     ]
+// // ]
+
+// // console.log(arr[2][1].data[2].schools[1])
+
+// // let arr = [
+// //     3,
+// //     '2',
+// //     {
+// //         "age": 14,
+// //         "car": "Toyota"
+// //     },
+// //     [
+// //         4,
+// //         7,
+// //         [
+// //             "first",
+// //             "second",
+// //             {
+// //                 "position": "third",
+// //                 "class": "JSS1C",
+// //             },
+// //         ],
+// //     ],
+// // ]
+
+// // console.log(typeof(arr[2].age))
+
+// // let people = ['Tope', 'Paul', "Tobi", "Rita", "Elvis"]
+
+// // console.log(people[0])
+// // console.log(people[1])
+// // console.log(people[2])
+// // console.log(people[3])
+// // console.log(people[4])
+
+// // let u = 1;
+
+// // for(i = 0; i < 10; i+2){
+// //     console.log(i)
+// // }
+
+// // console.log('Hello Tope')
+
+// // let index = 13
+
+// // console.log(`arr[${index}]`)
+
+// // console.log('arr[x]')
+
+// // let grade = 79
+
+// // console.log('arr[' + (80 - grade) + ']')
+
+// // let firstName = "John"
+// // let greeting = 'Hey'
+
+// // // console.log(greeting + ' ' + firstName)
+// // // console.log(`${greeting} ${firstName}`)
+
+// // let i = 0;
+
+// // // console.log('index[' + i + '][' + arr[i] + ']')
+
+// // // console.log(`index[${i}][${arr[i]}]`)
+
+// // for (z = 0; z < 31; z++) {
+// //     if (z == 0) {
+// //         console.log(`${z + 1}st position`)
 // //     }
-// // } else {
-// //     if (num < 100) {
-// //         console.log(`${num} is a small odd number`);
-// //     } else {
-// //         console.log(`${num} is a big odd number`);
+// //     else if (z == 1) {
+// //         console.log(`${z + 1}nd position`)
+// //     }
+// //     else if (z == 2) {
+// //         console.log(`${z + 1}rd position`)
+// //     }
+// //     else {
+// //         console.log(`${z + 1}th position`)
 // //     }
 // // }
 
-// let a = "Hello";
-// let s = ' ';
-// let b = "World";
-// let t = 7
-// console.log('Good morning, it is ' + (12 - t) + ' hours till afternoon')
-// console.log('Good morning, it is ${12-t} hours till afternoon')
-// console.log(`Good morning, it is ${12 - t} hours till afternoon`)
+// // let age = 19
 
-// // age = 12
-// // gender = 'male'
+// // let isOldEnough = 19 >= 18
 
-// // below 18
-// // male - fanta
-// // feale - coke
+// // console.log(isOldEnough)
 
-// // above 18
-// // male - wine
-// // fenale - beer
+// // if (i.toString() == 1) {
 
-// console.log('you are welcome, you can have a fanta')
-// console.log('you are welcome, you can have a coke')
+// // }
 
-// // Hello you are alloweed to take alcohol. Ple`se take a bottle of wine
+// // arr = [1, 2, 3, 4]
 
-// //
-// // isAlive = true/false
-// // dob = 1995
-// //
-// //
-// // if alive
-// // < 13 - you are alive. child
-// // 13 - 19 you are alive. teenager
-// // >19 - you are alive. adult
+// // total = 0
+// // num = 0
 
-// // < 13 - you are dead. child
-// // 13 - 19 you are dead. teenager
-// // >19 - you are dead. adult
+// // // console.log(`Adding 1 to total`)
+// // // console.log(`Adding 2 to total`)
+// // // console.log(`Adding 3 to total`)
+// // // console.log(`Adding 4 to total`)
+// // // console.log('The mean/avg num in the array is 2.5')
 
-// // index 0 - name
-// // index 1 - age
-// // index 2 - gender
-// let person1 = [
-//     'Tope',
-//     12,
-//     'Male',
-//     ['Tope', 21],
-//     { firstName: 'Tope' },
-// ]
-// let person2 = ['Tobi', 18, 'Female']
-// let person3 = ['Elvis', 40, 'Male']
+// // users = [
+// //     {
+// //         firstName: 'Tope',
+// //         dob: 2002,
+// //         gender: 'M',
+// //     },
+// //     {
+// //         firstName: 'Paul',
+// //         dob: 2003,
+// //         gender: 'M',
+// //     },
+// //     {
+// //         firstName: 'Melissa',
+// //         dob: 2002,
+// //         gender: 'F',
+// //     },
+// //     {
+// //         firstName: 'Precious',
+// //         dob: 2001,
+// //         gender: 'M',
+// //     },
+// //     {
+// //         firstName: 'Veronica',
+// //         dob: 2002,
+// //         gender: 'F',
+// //     },
+// //     {
+// //         firstName: 'Sophie',
+// //         dob: 2004,
+// //         gender: 'F',
+// //     },
+// // ]
 
-// let person4 = {
-//     firstName: "Elvis",
-//     age: 40,
-//     gender: "Male",
-//     children: [
-//         {
-//             firstName: 'Tope',
-//             dob: 2000,
-//             schools: ['KJA', 'KLC'],
-//         },
-//         {
-//             firstName: 'Jimi',
-//             age: 3,
-//             schools: null,
-//         }
-//     ]
-// }
+// // for (let i = 0; i < 6; i++) {
+// //     console.log(`${JSON.stringify(users[i])} == 6`)
+// // }
 
-// let arr = [
-//     'first',
-//     2,
-//     [
-//         false,
-//         {
-//             key: 'value',
-//             data: [
-//                 67,
-//                 78,
-//                 {
-//                     schools: ['test', 'lemon'],
-//                 },
-//             ],
-//         },
-//     ]
-// ]
+// // loop through the array
+// // while looping, print
 
-// console.log(arr[2][1].data[2].schools[1])
+// // Tope is 12 years old, and he is a boy
+// // Veronica is 12 years old, and she is a girl
+// // Precious is 11 years old, and he is a boy
 
-// let arr = [
-//     3,
-//     '2',
-//     {
-//         "age": 14,
-//         "car": "Toyota"
-//     },
-//     [
-//         4,
-//         7,
-//         [
-//             "first",
-//             "second",
-//             {
-//                 "position": "third",
-//                 "class": "JSS1C",
-//             },
-//         ],
-//     ],
-// ]
+// //outside of the loop
 
-// console.log(typeof(arr[2].age))
+// //The average age of users is: 13
 
-// let people = ['Tope', 'Paul', "Tobi", "Rita", "Elvis"]
-
-// console.log(people[0])
-// console.log(people[1])
-// console.log(people[2])
-// console.log(people[3])
-// console.log(people[4])
-
-// let u = 1;
-
-// for(i = 0; i < 10; i+2){
-//     console.log(i)
-// }
-
-// console.log('Hello Tope')
-
-// let index = 13
-
-// console.log(`arr[${index}]`)
-
-// console.log('arr[x]')
-
-// let grade = 79
-
-// console.log('arr[' + (80 - grade) + ']')
-
-// let firstName = "John"
-// let greeting = 'Hey'
-
-// // console.log(greeting + ' ' + firstName)
-// // console.log(`${greeting} ${firstName}`)
-
-// let i = 0;
-
-// // console.log('index[' + i + '][' + arr[i] + ']')
-
-// // console.log(`index[${i}][${arr[i]}]`)
-
-// for (z = 0; z < 31; z++) {
-//     if (z == 0) {
-//         console.log(`${z + 1}st position`)
-//     }
-//     else if (z == 1) {
-//         console.log(`${z + 1}nd position`)
-//     }
-//     else if (z == 2) {
-//         console.log(`${z + 1}rd position`)
-//     }
-//     else {
-//         console.log(`${z + 1}th position`)
-//     }
-// }
-
-// let age = 19
-
-// let isOldEnough = 19 >= 18
-
-// console.log(isOldEnough)
-
-// if (i.toString() == 1) {
-
-// }
-
-// arr = [1, 2, 3, 4]
-
-// total = 0
-// num = 0
-
-// // console.log(`Adding 1 to total`)
-// // console.log(`Adding 2 to total`)
-// // console.log(`Adding 3 to total`)
-// // console.log(`Adding 4 to total`)
-// // console.log('The mean/avg num in the array is 2.5')
-
-// users = [
-//     {
-//         firstName: 'Tope',
-//         dob: 2002,
-//         gender: 'M',
-//     },
-//     {
-//         firstName: 'Paul',
-//         dob: 2003,
-//         gender: 'M',
-//     },
-//     {
-//         firstName: 'Melissa',
-//         dob: 2002,
-//         gender: 'F',
-//     },
-//     {
-//         firstName: 'Precious',
-//         dob: 2001,
-//         gender: 'M',
-//     },
-//     {
-//         firstName: 'Veronica',
-//         dob: 2002,
-//         gender: 'F',
-//     },
-//     {
-//         firstName: 'Sophie',
-//         dob: 2004,
-//         gender: 'F',
-//     },
-// ]
-
-// for (let i = 0; i < 6; i++) {
-//     console.log(`${JSON.stringify(users[i])} == 6`)
-// }
-
-// loop through the array
-// while looping, print
-
-// Tope is 12 years old, and he is a boy
-// Veronica is 12 years old, and she is a girl
-// Precious is 11 years old, and he is a boy
-
-//outside of the loop
-
-//The average age of users is: 13
-
-//There are more boys than girls
-//There are more girls than boys
+// //There are more boys than girls
+// //There are more girls than boys
 
 const students = [
   {
@@ -351,13 +351,70 @@ const students = [
     firstName: "Sophie",
     dob: 2004,
   },
-
 ];
 
-function calculateAge(dateOfBirth) {
+// functionName(students);
+
+// function functionName(arr){
+//   console.log('The first position was arr[0].firstName')
+//   console.log('The last position was arr[arr.length - 1].firstName')
+// }
+
+// console.log('The first position was Tope')
+// console.log('The last position was Sophie')
+
+function calculateAge(dateOfBirth, ruw) {
   const currentYear = new Date().getFullYear();
   return currentYear - dateOfBirth;
 }
+
+// // function calculatePosition(position) {
+// //   if (position == 11 || position == 12 || position == 13) {
+// //     return `${position}th`;
+// //   }
+
+// //   const strPosition = position.toString();
+// //   const lastChar = strPosition[strPosition.length - 1];
+
+// //   if (lastChar == "1") {
+// //     return `${position}st`;
+// //   } else if (lastChar == "2") {
+// //     return `${position}nd`;
+// //   } else if (lastChar == "3") {
+// //     return `${position}rd`;
+// //   }
+// //   return `${position}th`;
+// // }
+
+// // for (var i = 0; i < students.length; i++) {
+// //   console.log(
+// //     `${students[i].firstName} is ${calculateAge(
+// //       students[i].dob
+// //     )} years old and was ${calculatePosition(i + 1)} position`
+// //   );
+// // }
+
+// // for(const student of students){
+// //   console.log(student.dob)
+// // }
+
+function makeTextLegible(text) {
+  console.log(`%c ${text}`, "font-size: 14px; color: orange; margin: 10px 0;");
+}
+
+function checkForNotfications() {
+  makeTextLegible(`This ran at ${new Date().toLocaleTimeString()}`);
+}
+
+// var x = setInterval(checkForNotfications, 1000);
+
+// setInterval(function () {
+//   clearInterval(x);
+// }, 5000);
+
+// var x = setTimeout(checkForNotfications, 1000);
+
+// position
 
 function calculatePosition(position) {
   if (position == 11 || position == 12 || position == 13) {
@@ -377,28 +434,34 @@ function calculatePosition(position) {
   return `${position}th`;
 }
 
+// let timer = 0;
 
-for (var i = 0; i < students.length; i++) {
-  console.log(
-    `${students[i].firstName} is ${calculateAge(
-      students[i].dob
-    )} years old and was ${calculatePosition(i + 1)} position`
-  );
-}
+// let v = setInterval(function () {
+//   timer += 1;
+//   makeTextLegible(
+//     `This is the ${calculatePosition(timer)} time this code is running`
+//   );
+//   if (timer >= 5) {
+//     clearInterval(v);
+//   }
+// }, 1000);
 
-for(const student of students){
-  console.log(student.dob)
-}
+// // const x = setInterval(function () {
+// //   timer += 1;
+// //   console.log(timer);
+// // }, 1000);
 
+// // setTimeout(function () {
+// //   clearInterval(x);
+// //   con
+// // }, 11);
 
+// // n => n/1000
 
+let timer = 0;
 
+setInterval(function(){
+  timer += 1
+  console.log(timer);
+}, 1000)
 
-
-
-
-
-
-console.log("%c ", "margin: 3rem;");
-
-//Sophie is [x] years old and was 11th position
---
+++ script1.js
@@ -0,0 +1,18 @@
+// const lis = document.getElementsByTagName("li");
+
+// for (let i = 0; i < lis.length; i++) {
+//   lis[i].classList.add("red");
+// }
+
+// setTimeout(function () {
+//   h1.style. = "80px";
+// }, 5500);
+
+const div = document.querySelector("div");
+
+setTimeout(function () {
+    div.style.width = '100%';
+    div.style.margin = '0';
+    div.style.outline = '5px double black';
+    div.style.outlineOffset = '25px'
+}, 1500);
